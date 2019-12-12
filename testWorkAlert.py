67f2abf211e05e7e66ce8915db2f093acfa4a1a0
IlyaShushlebin <il.0701@mail.ru>
2019-12-12 12:17:39 +0700

    Added testWorkAlert.py for 6task

---
+++ testWorkAlert.py
@@ -0,0 +1,39 @@
+import time
+import math
+
+# webdriver это и есть набор команд для управления браузером
+from selenium import webdriver
+
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+driver = webdriver.Chrome("C:/chromedriver/chromedriver.exe")
+
+# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+driver.get("http://suninjuly.github.io/alert_accept.html")
+
+# поис кнопки "I want to go on a magical journey!
+btn = driver.find_elements_by_css_selector(".btn")
+btn[0].click()
+
+# подтверждаем действие на модальном окне
+confirm = driver.switch_to.alert
+confirm.accept()
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+# Считываем значение х
+x = driver.find_element_by_id("input_value").text
+y = calc(x)
+
+# Ищем поле для ввода ответа
+textInputAnswer = driver.find_element_by_id("answer")
+textInputAnswer.send_keys(y)
+
+# Найдем кнопку submit
+btnSubmit = driver.find_element_by_css_selector(".btn") 
+btnSubmit.click()
+
+time.sleep(5)
+
+# После выполнения всех действий мы не должны забыть закрыть окно браузера
+driver.quit()
